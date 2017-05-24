eb07c6ad40c6b169dd4c3114efbcdc64284412f4
Ignacio Zunino <zignacio@gmail.com>
2017-05-24 11:19:41 -0300

    assessment screen + moving to another page when tapping on a row

--- Personal-Compass/ViewControllers/CreateCompassViewController.swift
+++ Personal-Compass/ViewControllers/CreateCompassViewController.swift
@@ -19,20 +19,47 @@ enum CompassError {
     case selection
 }
 
+enum CompassScene: String {
+    case stressor
+    case emotion
+    case thought
+    case body
+    case behavior
+    case assessment
+    
+    var color: UIColor {
+        switch self {
+        case .stressor:
+            return .darkGray
+        case .emotion:
+            return .red
+        case .thought:
+            return .blue
+        case .body:
+            return .orange
+        case .behavior:
+            return .green
+        case .assessment:
+            return .green
+        }
+    }
+    
+}
+
 class CreateCompassViewController: UIViewController {
     
-    private struct CompassItem {
+    fileprivate struct CompassItem {
         let viewController: UIViewController
         let scene: CompassScene
         var compass: Compass = Compass()
         
-        init(for scene: CompassScene, compass: Compass) {
+        init(for scene: CompassScene, container: CreateCompassViewController) {
             
             switch scene {
             case .body:
                 let viewController = UIStoryboard(name: "StressItems", bundle: nil).instantiateViewController(withIdentifier: "CompassStressViewController") as! CompassStressViewController
                 viewController.stressItemType = .body
-                viewController.currentCompass = compass
+                viewController.currentCompass = container.compass
                 viewController.title = "How is the stress of this situation affecting me physically?"
                 self.viewController = viewController
                 
@@ -40,49 +67,33 @@ class CreateCompassViewController: UIViewController {
                 let viewController = UIStoryboard(name: "StressItems", bundle: nil).instantiateViewController(withIdentifier: "CompassStressViewController") as! CompassStressViewController
                 viewController.title = "How are my thoughts about this situation affecting my behavior?"
                 viewController.stressItemType = .behaviour
-                viewController.currentCompass = compass
+                viewController.currentCompass = container.compass
                 self.viewController = viewController
                 
             case .stressor:
                 let viewController = UIStoryboard(name: scene.rawValue.capitalized, bundle: nil).instantiateInitialViewController() as! StressorViewController
-                viewController.currentCompass = compass
+                viewController.currentCompass = container.compass
                 self.viewController = viewController
                 
             case .emotion:
                 let viewController = UIStoryboard(name: scene.rawValue.capitalized, bundle: nil).instantiateInitialViewController() as! EmotionViewController
-                viewController.currentCompass = compass
+                viewController.currentCompass = container.compass
                 self.viewController = viewController
                 
             case .thought:
                 let viewController = UIStoryboard(name: scene.rawValue.capitalized, bundle: nil).instantiateInitialViewController() as! ThoughtViewController
-                viewController.currentCompass = compass
+                viewController.currentCompass = container.compass
                 self.viewController = viewController
+
+            case .assessment:
+                let viewController = UIStoryboard(name: scene.rawValue.capitalized, bundle: nil).instantiateInitialViewController() as! AssessmentViewController
+                viewController.currentCompass = container.compass
+                viewController.delegate = container
+                self.viewController = viewController
+
             }
             
             self.scene = scene
-        }
-    }
-    
-    private enum CompassScene: String {
-        case stressor
-        case emotion
-        case thought
-        case body
-        case behavior
-        
-        var color: UIColor {
-            switch self {
-            case .stressor:
-                return .darkGray
-            case .emotion:
-                return .red
-            case .thought:
-                return .blue
-            case .body:
-                return .orange
-            case .behavior:
-                return .green
-            }
         }
     }
     
@@ -95,14 +106,15 @@ class CreateCompassViewController: UIViewController {
 
     private var pageCount = 0
     
-    private lazy var compassItems: [CompassItem]  = {
+    fileprivate lazy var compassItems: [CompassItem]  = {
         
         var items: [CompassItem] = [
-            CompassItem(for: .stressor, compass: self.compass),
-            CompassItem(for: .emotion, compass: self.compass),
-            CompassItem(for: .thought, compass: self.compass),
-            CompassItem(for: .body, compass: self.compass),
-            CompassItem(for: .behavior, compass: self.compass),
+            CompassItem(for: .stressor, container: self),
+            CompassItem(for: .emotion, container: self),
+            CompassItem(for: .thought, container: self),
+            CompassItem(for: .body, container: self),
+            CompassItem(for: .behavior, container: self),
+            CompassItem(for: .assessment, container: self),
         ]
         
         return items
@@ -171,19 +183,24 @@ class CreateCompassViewController: UIViewController {
     
     
     // MARK: - User Actions
+    
+    fileprivate func moveToPage(page: Int, direction: UIPageViewControllerNavigationDirection) {
+        self.pageCount = page
+        self.pageControl.currentPage = self.pageCount
+        
+        let item = self.compassItems[self.pageCount]
+        
+        self.pageControlViewController.setViewControllers([item.viewController], direction: direction, animated: true, completion: nil)
+        self.setupLabel(for: item.scene)
+
+    }
 
     @IBAction func backAction(_ sender: UIButton) {
         guard self.pageCount > 0 else {
             return
         }
         
-        self.pageCount -= 1
-        self.pageControl.currentPage = self.pageCount
-        
-        let item = self.compassItems[self.pageCount]
-        
-        self.pageControlViewController.setViewControllers([item.viewController], direction: .reverse, animated: true, completion: nil)
-        self.setupLabel(for: item.scene)
+        moveToPage(page: self.pageCount - 1, direction: .reverse)
     }
     
     @IBAction func nextAction(_ sender: UIButton) {
@@ -203,14 +220,8 @@ class CreateCompassViewController: UIViewController {
             return
         }
 
-        self.pageCount += 1
-        
-        self.pageControl.currentPage = self.pageCount
-        
-        let item = self.compassItems[self.pageCount]
-        
-        self.pageControlViewController.setViewControllers([item.viewController], direction: .forward, animated: true, completion: nil)
-        self.setupLabel(for: item.scene)
+        moveToPage(page: self.pageCount + 1, direction: .forward)
+
     }
     
     @IBAction func cancelAction(_ sender: Any) {
@@ -230,5 +241,18 @@ class CreateCompassViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+}
+
+extension CreateCompassViewController: AssessmentViewControllerDelegate {
+
+    func didSelectScene(scene: CompassScene) {
+        
+        guard let scenePage = (compassItems.index { $0.scene == scene }) else {
+            return assertionFailure()
+        }
+        
+        self.moveToPage(page: scenePage, direction: .reverse)
+    }
+
 }
 
