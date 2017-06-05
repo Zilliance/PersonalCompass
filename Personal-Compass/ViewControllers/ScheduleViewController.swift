2914eb8aa3eeb9634d83ac1c085ea42f2d8683d9
Philip Dow <phil@phildow.net>
2017-06-05 10:20:16 -0700

    rename cancel to close in schedule scene

--- Personal-Compass/ViewControllers/ScheduleViewController.swift
+++ Personal-Compass/ViewControllers/ScheduleViewController.swift
@@ -18,7 +18,7 @@ class ScheduleViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(self.closeView))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Close", style: .plain, target: self, action: #selector(self.closeView))
         self.setupView()
     }
     
