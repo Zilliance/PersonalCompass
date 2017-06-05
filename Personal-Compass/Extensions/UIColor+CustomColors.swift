52d64706b35561ee56d7d662d542c94d15202180
Ignacio Zunino <zignacio@gmail.com>
2017-06-05 17:18:21 -0300

    fixed background color of summary cells

--- Personal-Compass/Extensions/UIColor+CustomColors.swift
+++ Personal-Compass/Extensions/UIColor+CustomColors.swift
@@ -23,6 +23,9 @@ extension UIColor {
     class func color(forRed red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
         return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: alpha)
     }
+    
+    static let contentBackground = UIColor.color(forRed: 250, green: 250, blue: 250, alpha: 1)
+
 }
 
 // MARK: - Scene Colors
