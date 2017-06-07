e68da23f27fd1b35902bbd49afa8036f8c2e16b1
Ignacio Zunino <zignacio@gmail.com>
2017-06-07 10:11:45 -0300

    added additional separator and changed to edited needed text on the wisdom summary screen

--- Personal-Compass/ViewControllers/InnerWisdomSummaryViewController.swift
+++ Personal-Compass/ViewControllers/InnerWisdomSummaryViewController.swift
@@ -47,6 +47,11 @@ class InnerWisdomSummaryViewController: UIViewController, UITableViewDelegate, U
         
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 84
+        
+        let additionalSeparator = UIView.init(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 2))
+        additionalSeparator.backgroundColor = UIColor.silverColor
+        
+        self.tableView.tableHeaderView = additionalSeparator
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -85,7 +90,7 @@ class InnerWisdomSummaryViewController: UIViewController, UITableViewDelegate, U
             cell = tableView.dequeueReusableCell(withIdentifier: "CompassFacetSummaryCell", for: indexPath) as! CompassFacetSummaryCell
 
             cell.title.text = "Need"
-            cell.label.text = currentCompass.need
+            cell.label.text = currentCompass.editedNeed
             cell.label.textColor = UIColor.innerWisdom
 
         case .actionStep:
