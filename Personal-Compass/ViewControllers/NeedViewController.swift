e323536af0ed8f9e2b384c6a358bdc70fadd301b
Philip Dow <phil@phildow.net>
2017-06-13 10:26:20 -0700

    need uses KMPlaceholderTextView

--- Personal-Compass/ViewControllers/NeedViewController.swift
+++ Personal-Compass/ViewControllers/NeedViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import KMPlaceholderTextView
 
 class NeedViewController: AutoscrollableViewController {
 
     var currentCompass: Compass!
     
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textView: KMPlaceholderTextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,13 +32,7 @@ class NeedViewController: AutoscrollableViewController {
         self.textView.layer.borderWidth = App.Appearance.borderWidth
         self.textView.layer.borderColor = UIColor.lightGray.cgColor
         
-        self.setupPlaceholderTextView(self.textView, placeholder: self.placeholder(for: self.textView), attributes: [
-            NSFontAttributeName: UIFont.muliLight(size: 14),
-            NSForegroundColorAttributeName: self.placeholderTextColor(for: self.textView)
-        ])
-        
         if let need = self.currentCompass.need {
-            self.textView.textColor = self.normalTextColor(for: textView)
             self.textView.text = need
         }
     }
@@ -48,9 +43,7 @@ class NeedViewController: AutoscrollableViewController {
 
 extension NeedViewController: CompassFacetEditor {
     func save() {
-        if self.textView.text != self.placeholder(for: self.textView) {
-            self.currentCompass.need = self.textView.text
-        }
+        self.currentCompass.need = self.textView.text
         self.currentCompass.lastEditedFacet = .need
     }
 }
@@ -59,7 +52,7 @@ extension NeedViewController: CompassFacetEditor {
 
 extension NeedViewController: CompassValidation {
     var error: CompassError? {
-        if self.textView.text.isEmpty || self.textView.text == self.placeholder(for: self.textView) {
+        if self.textView.text.isEmpty {
             return .text
         } else {
             return nil
@@ -67,38 +60,15 @@ extension NeedViewController: CompassValidation {
     }
 }
 
-// MARK: - TextViewPlaceholder
-
-extension NeedViewController: TextViewPlaceholder {
-    func placeholder(for textView: UITextView) -> String {
-        return "For example, I need a job, I need my friend to understand how I feel, or I need some quiet downtime to recharge."
-    }
-    func placeholderTextColor(for textView: UITextView) -> UIColor {
-        return .textPlaceholderColor
-    }
-    func normalTextColor(for textView: UITextView) -> UIColor {
-        return .textInputColor
-    }
-}
-
 //MARK: - UITextViewDelegate
 
 extension NeedViewController: UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if (text == "\n")
-        {
+        if (text == "\n") {
             textView.resignFirstResponder()
             return false
         }
         
         return true
-    }
-    
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        self.placeholderTextViewDidBeginEditing(textView)
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        self.placeholderTextViewDidEndEditing(textView)
     }
 }
