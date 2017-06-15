2e34fe2e6f24135162b56ec58369dd0e973e6d2a
Ricardo Hernandez <a.ricardohg@gmail.com>
2017-06-15 15:21:00 -0500

    added new need / summary colors

--- Personal-Compass/Extensions/UIColor+CustomColors.swift
+++ Personal-Compass/Extensions/UIColor+CustomColors.swift
@@ -44,9 +44,10 @@ extension UIColor {
     static let stressor = UIColor.color(forRed: 243, green: 135, blue: 32, alpha: 1)
     static let emotion = UIColor.color(forRed: 234, green: 91, blue: 67, alpha: 1)
     static let thought = UIColor.color(forRed: 0, green: 184, blue: 231, alpha: 1)
+    static let need = UIColor.color(forRed: 243, green: 135, blue: 32, alpha: 1)
     static let body = UIColor.color(forRed: 255, green: 184, blue: 55, alpha: 1)
     static let behavior = UIColor.color(forRed: 101, green: 175, blue: 71, alpha: 1)
-    static let summary = UIColor.color(forRed: 234, green: 91, blue: 67, alpha: 1)
+    static let summary = UIColor.color(forRed: 41, green: 123, blue: 247, alpha: 1)
     static let innerWisdom = UIColor.color(forRed: 98.0, green: 109.0, blue: 218.0, alpha: 1)
     static let feelBetter = UIColor.color(forRed: 98.0, green: 109.0, blue: 218.0, alpha: 1)
 }
